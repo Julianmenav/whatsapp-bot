2ff79bc6eebc8080fcbe4ae9d40e19316a4e7dad
juli <julianmenav2@gmail.com>
2023-05-30 18:58:17 +0200

    chore: setup gitignore

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const qrcode = require('qrcode-terminal');
-
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require("qrcode-terminal");
+const contacts = require("./contacts.json");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
-    authStrategy: new LocalAuth()
-});
- 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+  authStrategy: new LocalAuth(),
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('message', message => {
-    console.log(message.body)
-    console.log(message.from)
-    if(message.from == '34680953843-1405427733@g.us'){
-        client.sendMessage(message.from, `* hello world*`);
-    }
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
 
-})
+client.on("message", (message) => {
+  console.log({ [message.from]: message.body });
+  if (message.from == contacts.sujeto) {
+    client.sendMessage(message.from, `* hello world *`);
+  }
+});
 
 client.initialize();
- 
